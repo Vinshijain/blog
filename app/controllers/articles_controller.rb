f7cd741a44f38b6e494c84871bdceefbd2dea9fb
Vinshijain <vinshujain0122@gmail.com>
2017-06-04 00:14:50 +0530

    added destroy action and done with CRUD action in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -31,6 +31,12 @@ def update
     render 'edit'
   end
   end
+  def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+ 
+  redirect_to articles_path
+end
  
 private
   def article_params
