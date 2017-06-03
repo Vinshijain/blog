8ac86fe96c0d68b59c225f2a5b4832f2d258c11a
Vinshijain <vinshujain0122@gmail.com>
2017-06-03 23:48:56 +0530

    added an edit action in ArticlesController

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,31 @@
 class ArticlesController < ApplicationController
-	def index
+	 def index
     @articles = Article.all
   end
-	def show
+ 
+  def show
     @article = Article.find(params[:id])
   end
-  def new
-  	@article = Article.new
-	end
-	def create
+
+def new
+  @article = Article.new
+end
+ def edit
+  @article = Article.find(params[:id])
+end
+def create
   @article = Article.new(article_params)
  
-  @article.save
-  redirect_to @article
-else 
-	render 'new'
+  if @article.save
+    redirect_to @article
+  else
+    render 'new'
+  end
 end
  
 private
   def article_params
     params.require(:article).permit(:title, :text)
   end
-end
+  end
+
