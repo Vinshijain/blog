b0e7c7f5e93e279b2e5b5d8f42d5af18fdc011b1
Vinshijain <vinshujain0122@gmail.com>
2017-06-05 11:10:36 +0530

    modified username and password

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  http_basic_authenticate_with name: "dhh", password: "secret",
+  http_basic_authenticate_with name: "Vinshi", password: "secret",
  only: :destroy
   def create
     @article = Article.find(params[:article_id])
