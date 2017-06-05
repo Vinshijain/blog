7839617ae64e8c4166be0cd3d23ab994e5715562
Vinshijain <vinshujain0122@gmail.com>
2017-06-05 11:56:25 +0530

    fixed indentation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
-  http_basic_authenticate_with name: "Vinshi", password: "secret",
- only: :destroy
+  http_basic_authenticate_with name: "Vinshi", password: "secret", only: :destroy
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
@@ -15,7 +14,7 @@ class CommentsController < ApplicationController
   end
  
   private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body)
-    end
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
 end
