956574af5760f4250906b830cd8b83b7e3a13951
Vinshijain <vinshujain0122@gmail.com>
2017-06-05 11:07:24 +0530

    rectified the errors in article model and done http authentication

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
-class Artilce < ActiveRecord::Base
+class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
